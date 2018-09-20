3bb0ece3f10160012f5ca8ad2d00bbc271ce42b3
Brian Byers <brian@xclo.io>
2018-09-20 17:55:03 -0500

    simplifying blue/green

--- ci/tasks/restart-artifactory.sh
+++ ci/tasks/restart-artifactory.sh
@@ -3,13 +3,12 @@ set -e
 
 #clone the repo
 git clone https://$PASSWORD@$GIT_REPO_URL
-git clone --single-branch -b color https://$PASSWORD@$GIT_REPO_URL colorBranch
-
-#meow the color
-color=`cat colorBranch/color`
 
 # make it colorful!
 cd s1p-concourse-spring-music
+
+#meow the color
+color=`cat color`
 cd bin
 
 #add git config so it stops complaining about not having user info
@@ -22,16 +21,7 @@ echo "Current color: "$color
 if [ "$color" == "blue" ]; then
   echo "turning banner green"
   ./makeItGreen.sh
-  echo "green" > ../../colorBranch/color
 else
   echo "turning banner blue"
   ./makeItBlue.sh
-  echo "blue" > ../../colorBranch/color
 fi
-
-echo "New color to save"
-newColor=`cat ../../colorBranch/color`
-cd ../../colorBranch
-git add color
-git commit -m "saving color $newColor"
-git push
